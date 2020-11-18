dd22b9bee1749eb09c86f8fe6b80915fb9e8eb9c
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-11-18 19:40:58 +0800

    update contact

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -47,7 +47,7 @@ module.exports = {
         "items": [
           {
             "text": "GitHub",
-            "link": "https://github.com/recoluan",
+            "link": "https://github.com/lz1998",
             "icon": "reco-github"
           }
         ]
